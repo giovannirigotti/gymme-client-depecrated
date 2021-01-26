0248439dae933a14f990a33664ab519a3a1c49eb
fbafkis <Elgarfinn69!>
2021-01-26 17:43:38 +0100

    Separazione signup vari utenti iniziata.

---
+++ app/src/main/java/android_team/gymme_client/signup/CustomerSignupActivity.java
@@ -0,0 +1,17 @@
+package android_team.gymme_client.signup;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android_team.gymme_client.R;
+
+public class CustomerSignupActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_signup_customer);
+    }
+
+}
