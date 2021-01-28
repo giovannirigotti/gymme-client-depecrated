d71305fc940bf340290f6645de7b31f91822ad3a
giovannirigotti <rigotti.giovanni@gmail.com>
2021-01-28 16:40:17 +0100

    iniziato drawer menu e caricato i loghi

---
+++ app/src/main/java/android_team/gymme_client/trainer/TrainerHomeActivity.java
@@ -0,0 +1,17 @@
+package android_team.gymme_client.trainer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import android_team.gymme_client.R;
+
+public class TrainerHomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trainer_home);
+    }
+
+}
